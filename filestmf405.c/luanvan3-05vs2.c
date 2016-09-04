321596cc6c362287a6f234bca0047fb239113538
ChucVanLe <levanchuc94qn@gmail.com>
2016-09-04 20:05:14 +0700

    remove variable for save data to SD card

--- filestmf405.c/luanvan3-05vs2.c
+++ filestmf405.c/luanvan3-05vs2.c
@@ -30,13 +30,7 @@
 
 /**************************************************************************************/
 //bien cho ham sd card
-FRESULT st_Result = FR_OK;   /* Result code */  
-FATFS fatfs;  /* File system object */  
-FIL fil;   /* File object */  
-DIR dir;   /* Directory object */  
-FILINFO fno;  /* File information object */  
-UINT *bw;
-const TCHAR *FilePath = "0:/HTDKN2/STM32.TXT" ; // file path 
+
 /**************************************************************************************/
 uint8_t CMD_Trigger =0;
 uint8_t Alt_latest =0;
@@ -89,9 +83,7 @@ int main(void)
     power ();
     
     MyTIM_PWM_Configuration();  
-    
-    f_mount(0, &fatfs);  
-		f_mkdir ("0:/HTDKN2");    
+       
     SysTick_Config(SystemCoreClock/500);
     GPIO_SetBits(GPIOB,GPIO_Pin_12);
 
@@ -134,102 +126,70 @@ void SysTick_Handler(void)
     if(count1==count2)
     {
         y=300-count2;//y number byte in buffer DMA has received
-        if (y>10)//buffer size IMU/GPS < 290
+        if (y>120)//buffer size IMU/GPS < 290
         {
              //fastest way to restart DMA
             DMA_Cmd(DMA1_Stream5,DISABLE);
             DMA_ClearFlag(DMA1_Stream5, DMA_FLAG_TCIF5);
             DMA_SetCurrDataCounter(DMA1_Stream5,300);
             DMA_Cmd(DMA1_Stream5,ENABLE);   
-            if(y>100)//buffer size GPS < 200
+            //if(y>100)//buffer size GPS < 200
             {           
-//                    for(i=0;i<y;i++)
-//                    {
-//                        dem=0;
-//                        if(Buf_USART2[i]=='$')
-//                        {
-//													//if i1 == 0;
-//													//$ dau tien la chuoi  GGA, vi tri $ la h, vi tri ket thuc la l
-//													//$ thu 2 la chuoi VTG,vi tri ket thuc la l1
-//                            i1++;
-//                            if(i1==1)
-//                            {
-//                                h=i;//luu vi tri GGA_$                                                          
-//                            }
-//                        }
-//                        if((Buf_USART2[i]==13)&&(i1==1))
-//                        {
-//                            l=i;//luu vi tri GGA_13
-//                        }
-//                        if((Buf_USART2[i]==13)&&(i1==2))
-//                        { 
-//                            l1=i;//luu vi tri VTG_13
-//                            i1=0;
-//                            break;
-//                        }
-//                    }
-//                    for(i=0;i<80;i++)
-//                    {
-//                        IMU[i]=Buf_USART2[i];
-//                    }               
-//                    Sampling_RPY(Buf_USART2,80);//get roll, pitch, yaw
-//                    Sampling_GGA(&Buf_USART2[h-1] ,l-h+2);//get lat, lon
-//                    //Sampling_GGA(GGA1 ,26);
-//                    Sampling_VTG(&Buf_USART2[l+1],l1-l);//get speed
-//                    flag=1;//save data to SD card
-//                    //imu ( DATA[0--> h-2]  BAO GOM \N VA \S\R)
-//                    if ( CMD_Trigger == 0)//if CMD_Trigger =1 ; receive data from ground station
-//                    {//if CMD_Trigger = 0 ; transmit data to ground station
-//                            Buf_UART4[0]=10;
-//                                //Buf_UART4[1]=crc;
-//                            Buf_UART4[2]=32;
-//                            Buf_UART4[3]=73;//I
-//                            Buf_UART4[4]=32;
-//                            for(i=1;i<h-3;i++)
-//                            {
-//                                Buf_UART4[i+4]=Buf_USART2[i];   
-//                            }
-//                            Buf_UART4[h+1]=13;   
-//                            crc=CRC_Cal(3,Buf_UART4,h); 
-//                            Buf_UART4[1]=crc;
-//                
-//                            //GGA ( DATA[h-1-->l] BAO GOM \N VA \R )
-//                            crc=CRC_Cal(h,Buf_USART2,l-1);
-//                            Buf_UART4[h+2]=10;
-//                            Buf_UART4[h+3]=crc;
-//                            Buf_UART4[h+4]=32;
-//                            for(i=h;i<l+1;i++)
-//                            {
-//                                Buf_UART4[i+5]=Buf_USART2[i];
-//                            }
-//                            
-//                            //VTG ( DATA[l+1 --> x-1] bao gom \n va \r )
-//                            crc=CRC_Cal(l+2,Buf_USART2,y-2);
-//                            Buf_UART4[l+6]=10;
-//                            Buf_UART4[l+7]=crc;
-//                            Buf_UART4[l+8]=32;
-//                            for(i=l+2;i<y;i++)
-//                            {
-//                                    Buf_UART4[i+7]=Buf_USART2[i];
-//                            }
-//                            
-//                            DMA_SetCurrDataCounter(DMA1_Stream4,y+7);
-//                            DMA_Cmd(DMA1_Stream4,ENABLE);   
-//                        }
-//                        else
-//                        {
-//                            CMD_Trigger=0;
-//                            xacnhan(&Buf_rx4[0]);
-//                            CMD_delay =1 ;
-////                          DMA_Cmd(DMA1_Stream2,DISABLE);
-////                          DMA_ClearFlag(DMA1_Stream2, DMA_FLAG_TCIF2);
-////                          DMA_SetCurrDataCounter(DMA1_Stream2,9);
-////                          DMA_Cmd(DMA1_Stream2,ENABLE);
-//                        }
+                    for(i=0;i<y;i++)
+                    {
+                        dem=0;
+                        if(Buf_USART2[i]=='$')
+                        {
+													//if i1 == 0;
+													//$ dau tien la chuoi  GGA, vi tri $ la h, vi tri ket thuc la l
+													//$ thu 2 la chuoi VTG,vi tri ket thuc la l1
+                            i1++;
+                            if(i1==1)
+                            {
+                                h=i;//luu vi tri GGA_$                                                          
+                            }
+                        }
+                        if((Buf_USART2[i]==13)&&(i1==1))
+                        {
+                            l=i;//luu vi tri GGA_13
+                        }
+                        if((Buf_USART2[i]==13)&&(i1==2))
+                        { 
+                            l1=i;//luu vi tri VTG_13
+                            i1=0;
+                            break;
+                        }
+                    }
+                    for(i=0;i<80;i++)
+                    {
+                        IMU[i]=Buf_USART2[i];
+                    }               
+                    Sampling_RPY(Buf_USART2,80);//get roll, pitch, yaw
+                    Sampling_GGA(&Buf_USART2[h-1] ,l-h+2);//get lat, lon
+                    //Sampling_GGA(GGA1 ,26);
+                    Sampling_VTG(&Buf_USART2[l+1],l1-l);//get speed
+                    flag=1;//save data to SD card
+                    //imu ( DATA[0--> h-2]  BAO GOM \N VA \S\R)
+                    if ( CMD_Trigger == 0)//if CMD_Trigger =1 ; receive data from ground station
+                    {//if CMD_Trigger = 0 ; transmit data to ground station
+                            
+                            DMA_SetCurrDataCounter(DMA1_Stream4,y);
+                            DMA_Cmd(DMA1_Stream4,ENABLE);   
+                        }
+                        else
+                        {
+                            CMD_Trigger=0;
+                            xacnhan(&Buf_rx4[0]);
+                            CMD_delay =1 ;
+//                          DMA_Cmd(DMA1_Stream2,DISABLE);
+//                          DMA_ClearFlag(DMA1_Stream2, DMA_FLAG_TCIF2);
+//                          DMA_SetCurrDataCounter(DMA1_Stream2,9);
+//                          DMA_Cmd(DMA1_Stream2,ENABLE);
+                        }
                     
                 }
                    
-            else	//only data of IMU
+            //else	//only data of IMU
             {   
              //becase data from IMU/GPS send all of data (201 byte) in th one transmit
 							//user does not process in case  only data of IMU
